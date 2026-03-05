35783b35435a3d14f898423b5d7fdaf359165a9f
swamini <swaminibhandare10@gmail.com>
2026-03-05 08:25:21 +0530

    Building Get All Employee REST API

--- springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -2,12 +2,36 @@ package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
 
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+    @Autowired
+	private EmployeeRepository employeeRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+		Employee employee = new Employee();
+		employee.setFirstName("Swami");
+		employee.setLastName("Bhandare");
+		employee.setEmailId("swamini@10gmail.com");
+		employeeRepository.save(employee);
+
+		Employee employee1 = new Employee();
+		employee1.setFirstName("Soham");
+		employee1.setLastName("Mali");
+		employee1.setEmailId("soham@10gmail.com");
+		employeeRepository.save(employee1);
+
 	}
 
 }
