d71a529eba7b2979d3092cc5bfaf1ace9ae711f9
swamini <swaminibhandare10@gmail.com>
2026-04-04 19:38:39 +0530

    Creating React Header and Footer in React App

---
+++ react-frontend/src/components/FooterComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+const FooterComponent = () => {
+    return (
+        <div>
+            <footer className='footer'>
+                <p className='text-muted'>All Rights Reserved 2026@Swamini</p>
+            </footer>
+        </div>
+    )
+}
+
+
+export default FooterComponent
---
+++ react-frontend/src/components/HeaderComponent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const HeaderComponent = () => {
+  return (
+    <div>
+        <header className='header'>
+            <nav className='navbar navbar-expand-md navbar-dark '>
+           <div>
+            <a href='https://github.com/swamini10' className='navbar-brand'>
+              Employee Management System
+            </a>
+           </div>
+            </nav>
+        </header>
+    </div>
+  )
+}
+export default HeaderComponent
