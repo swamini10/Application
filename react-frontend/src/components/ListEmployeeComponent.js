cae6d24c621072bc494f47b4642fb54eaa41886b
swamini <swaminibhandare10@gmail.com>
2026-04-04 18:32:59 +0530

    Connect React to Get All Employee REST API

--- react-frontend/src/components/ListEmployeeComponent.js
+++ react-frontend/src/components/ListEmployeeComponent.js
@@ -1,8 +1,18 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
+import EmployeeService from '../services/EmployeeService'
 
 const ListEmployeeComponent = () => {
 
     const[employees, setEmployees]=useState([])
+
+useEffect(() => {
+    EmployeeService.getAllEmployees().then((response) => {
+        setEmployees(response.data);
+        console.log(response.data);
+    }).catch(error => {
+        console.log(error);
+    })
+}, [])
 
   return (
     <div className='container'> 
--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -3,13 +3,15 @@ package net.javaguides.springboot.controller;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+//@CrossOrigin("*")//this used to acess all the api from any domain
+@CrossOrigin(origins = "http://localhost:3000")//this used to acess all the api from only this domain
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeController {
