ecb60b924b2578b3097da1ba20ea9d26920afa99
swamini <swaminibhandare10@gmail.com>
2026-04-05 08:35:08 +0530

    solve CORS issue

--- react-frontend/src/components/ListEmployeeComponent.js
+++ react-frontend/src/components/ListEmployeeComponent.js
@@ -1,5 +1,6 @@
-import React,{useState,useEffect} from 'react'
-import EmployeeService from '../services/EmployeeService'
+import React, { useState, useEffect } from 'react';
+import EmployeeService from '../services/EmployeeService';
+import { Link } from 'react-router-dom';
 const ListEmployeeComponent = () => {
 
     const[employees, setEmployees]=useState([])
@@ -14,28 +15,30 @@ useEffect(() => {
 }, [])
 
   return (
-    <div className='container'> 
-        <h2 className='text-center'>Employees List</h2>
-        <link to ="/add-employee" className='btn btn-primary mb-2'>Add Employee</link>
-        <table className='table table-striped table-bordered'>
-            <thead>
-                    <th>Employee ID</th>
-                    <th>Employee First Name</th>
-                    <th>Employee Last Name</th>
-                    <th>Employee Email</th>
-            </thead>
-            <tbody>
-                {employees.map(employee => (
-                    <tr key={employee.id}>
-                        <td>{employee.id}</td>
-                        <td>{employee.firstName}</td>
-                        <td>{employee.lastName}</td>      
-                        <td>{employee.emailId}</td>
+        <div className='container'>
+            <h2 className='text-center'>List Employees</h2>
+            <Link to="/add-employee" className='btn btn-primary mb-2'>Add Employee</Link>
+            <table className='table table-striped table-bordered'>
+                <thead>
+                    <tr>
+                        <th>Employee Id</th>
+                        <th>Employee First Name</th>
+                        <th>Employee Last Name</th>
+                        <th>Employee Email Id</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
-    </div>
+                </thead>
+                <tbody>
+                    {employees.map(employee => (
+                        <tr key={employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.lastName}</td>
+                            <td>{employee.emailId}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
   )
 }
 
